e034c6bd11d0f1bd0363e71d0f3005d8fdef4061
unknown <nguyenkhanhdn94@gmail.com>
2024-05-31 10:12:33 -0400

    init project

---
+++ src/kec/HelloWorld.java
@@ -0,0 +1,10 @@
+package kec;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello World with dit me may");
+		System.out.println("Hello World with dit me may");
+		System.out.println("Hello World with dit me may");
+		System.out.println("Hello World with dit me may");
+	}
+}
